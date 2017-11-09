5aa8f8c9b52d92030227f86fd34a2d3186a5cb3f
mren-ext-aa <mren-ext.appannie.com>
2017-11-09 17:12:21 +0800

    Demo6:图像处理

--- demo6.py
+++ demo6.py
@@ -1,30 +1,16 @@
-#coding: utf-8
-import cmd
-# 存放敏感词文件的路径
-filtered_words_filepath = '/Users/mren-ext/Documents/filtered_words.txt'
-class CLI(cmd.Cmd):
-
-    def __init__(self): #初始基础类方法
-        cmd.Cmd.__init__(self)  # 初始化，提取敏感词列表
-        self.intro = 'Python敏感词检测:' #输出欢迎信息
-        f = open(filtered_words_filepath)
-        self.words = list(map(lambda i: i.strip('\n'), f.readlines()))
-        print(self.words)
-        self.prompt = ">>> "    # 定义提示符
-
-    def default(self, line):
-        if any([i in line for i in self.words]):
-            print ('****')
-        else:
-            print (line)
-
-    def do_quit(self, arg):
-        exit()
-        return True
-
-if __name__ =="__main__":
-    cli = CLI()
-    cli.cmdloop()
-
-# print(any([0,1]))
-# print(all([0,1]))
+# PIL:Python图像处理，需pip3 install pillow
+# ImageFont字体，ImageDraw画图
+from PIL import Image, ImageDraw, ImageFont
+def add_num(img):
+    draw = ImageDraw.Draw(img)
+    # ImageFont.truetype()设置字体和大小，.ttf：包含TureType字体的OpenType文件后缀名为.ttf
+    myfont = ImageFont.truetype('Arial.ttf', size=140)
+    fillcolor = "#ff0000"
+    width, height = img.size
+    draw.text((width/2-70, height/2-70), '99', font=myfont, fill=fillcolor)
+    img.save('result.jpg','jpeg')
+    return 0
+if __name__ == '__main__':
+    image = Image.open('a.jpg')
+    add_num(image)
+    image.show()
